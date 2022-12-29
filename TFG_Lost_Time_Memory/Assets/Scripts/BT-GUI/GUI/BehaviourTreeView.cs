a2f8f45111219c295c6c457bd934cbcad1514e35
Jorge <jgaliano2000@gmail.com>
2022-12-29 14:31:33 +0100

    #BT Behaviour Tree editor finished

--- TFG_Lost_Time_Memory/Assets/Scripts/BT-GUI/GUI/BehaviourTreeView.cs
+++ TFG_Lost_Time_Memory/Assets/Scripts/BT-GUI/GUI/BehaviourTreeView.cs
@@ -1,5 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.Experimental.GraphView;
+using UnityEngine;
 using UnityEngine.UIElements;
 
 public class BehaviourTreeView : GraphView
@@ -10,14 +13,14 @@ public class BehaviourTreeView : GraphView
 
     public BehaviourTreeView()
     {
-        Insert(0, new GridBackground());
+        Insert(0,new GridBackground());
         
         this.AddManipulator(new ContentZoomer());
         this.AddManipulator(new ContentDragger());
         this.AddManipulator(new SelectionDragger());
         this.AddManipulator(new RectangleSelector());
         
-        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/BT-GUI/GUI/BehaviourTreeEditor.uss");
+        var styleSheet = AssetDatabase.LoadAssetAtPath<StyleSheet>("Assets/Scripts/BT-GUI/GUI/BT-Editor/BehaviourTreeEditor.uss");
         styleSheets.Add(styleSheet);
     }
 }
--- TFG_Lost_Time_Memory/Assets/Scripts/BT-GUI/GUI/InspectorView.cs
+++ TFG_Lost_Time_Memory/Assets/Scripts/BT-GUI/GUI/InspectorView.cs
@@ -1,12 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
 using UnityEngine.UIElements;
 
 public class InspectorView : VisualElement
 {
-    public new class UxmlFactory : UxmlFactory<InspectorView, GraphView.UxmlTraits>
+    public new class UxmlFactory : UxmlFactory<InspectorView, UxmlTraits>
     {
     }
     public InspectorView()
